0cceb9bf10669344fdc6a7fa8c69dd4814df478e
Nico Kratky <nico@nicokratky.me>
2023-12-19 16:53:30 +0100

    feat(vim): add barbar
    Signed-off-by: Nico Kratky <nico@nicokratky.me>

---
+++ dot_config/nvim/lua/plugins/barbar.lua
@@ -0,0 +1,21 @@
+return {
+  "romgrk/barbar.nvim",
+  dependencies = {
+    "lewis6991/gitsigns.nvim",
+    "nvim-tree/nvim-web-devicons",
+  },
+  init = function()
+    vim.g.barbar_auto_setup = false
+  end,
+  opts = {},
+  version = "^1.0.0",
+  lazy = false,
+  keys = {
+    {"<Tab>", "<cmd>BufferNext<cr>"},
+    {"<S-Tab>", "<cmd>BufferPrevious<cr>"},
+    {"<leader><Tab>", "<cmd>BufferPick<cr>"},
+    {"<leader><S-Tab>", "<cmd>BufferPickDelete<cr>"},
+    {"<leader>c", "<cmd>BufferClose<cr>"},
+    {"<S-t>", "<cmd>tabnew<cr>"},
+  }
+}
