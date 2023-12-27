bd83bc7c9685471d6bc375fc1d505d9af16e291b
Nico Kratky <nico@nicokratky.me>
2023-12-27 12:08:01 +0100

    feat(nvim): add lsp
    Signed-off-by: Nico Kratky <nico@nicokratky.me>

---
+++ dot_config/nvim/lua/plugins/lsp-config.lua
@@ -0,0 +1,28 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    lazy = false,
+    opts = {}
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    opts = {
+      ensure_installed = {
+        "lua_ls"
+      }
+    }
+  },
+  {
+    "neovim/nvim-lspconfig",
+    lazy = false,
+    config = function()
+      local lspconfig = require("lspconfig")
+      lspconfig.lua_ls.setup({})
+
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "<leader>gr", vim.lsp.buf.references, {})
+      vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+    end
+  }
+}
