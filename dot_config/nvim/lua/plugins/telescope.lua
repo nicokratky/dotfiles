ae7b3b16b24f826058eed2691a91bf437c348fdc
Nico Kratky <nico@nicokratky.me>
2023-12-29 18:06:49 +0100

    refactor(nvim): remove search git files keybind
    Signed-off-by: Nico Kratky <nico@nicokratky.me>

--- dot_config/nvim/lua/plugins/telescope.lua
+++ dot_config/nvim/lua/plugins/telescope.lua
@@ -30,7 +30,6 @@ return {
       vim.keymap.set("n", ";", builtin.buffers)
       vim.keymap.set("n", "<leader>sf", builtin.find_files, { desc = "[S]earch [F]iles" })
       vim.keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "[S]earch by [G]rep" })
-      vim.keymap.set("n", "<leader>gf", builtin.git_files, { desc = "Search [Git] [F]iles" })
       vim.keymap.set("n", "<leader>?", builtin.oldfiles, { desc = "[?] Find recently opened files" })
 
       require("telescope").load_extension("ui-select")
