2efbe38639a9029f24a8d4420c3805a5a364c700
Nico Kratky <nico@nicokratky.me>
2024-01-04 20:27:57 +0100

    feat(nvim): add mason-null-ls
    Signed-off-by: Nico Kratky <nico@nicokratky.me>

--- dot_config/nvim/lua/plugins/none-ls.lua
+++ dot_config/nvim/lua/plugins/none-ls.lua
@@ -1,8 +1,17 @@
 return {
+	{
 		"nvimtools/none-ls.nvim",
+		dependencies = {
+			"jay-babu/mason-null-ls.nvim",
+		},
 		config = function()
 			local null_ls = require("null-ls")
+			local mason_null_ls = require("mason-null-ls")
 
+			mason_null_ls.setup({
+				ensure_installed = nil,
+				automatic_installation = true,
+			})
 
 			null_ls.setup({
 				sources = {
@@ -11,6 +20,8 @@ return {
 					null_ls.builtins.formatting.terraform_fmt,
 				},
 			})
+
 			vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 		end,
+	},
 }
