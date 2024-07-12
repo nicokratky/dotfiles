e2a1f3a05bd8f892568b4ea3a7c6dafe65dd79eb
Nico Kratky <nico@nicokratky.me>
2024-07-12 18:52:03 +0200

    feat: add pinentry-mac
    Signed-off-by: Nico Kratky <nico@nicokratky.me>

--- .chezmoiscripts/run_once_after_setup-general.sh
+++ .chezmoiscripts/run_once_after_setup-general.sh
@@ -3,3 +3,4 @@
 mkdir -p $HOME/.gnupg
 touch $HOME/.gnupg/gpg-agent.conf
 echo enable-ssh-support >> $HOME/.gnupg/gpg-agent.conf
+echo pinentry-program /opt/homebrew/bin/pinentry-mac >> $HOME/.gnupg/gpg-agent.conf
